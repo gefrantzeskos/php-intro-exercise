f6fca9af8266dc30dfe09b9c660fe6f9c6840132
gefrantzeskos <gefrantzeskos@gmail.com>
2014-10-18 00:44:48 +0300

    php exercise
    php exercise

---
+++ index.php
@@ -0,0 +1,47 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>php exercise1</title>
+<style type="text/css">
+.style1 {
+	text-align: left;
+}
+</style>
+</head>
+
+<body>
+
+
+
+
+<form method="post" action="search.php">
+
+
+
+
+<div class="style1">
+
+
+
+
+Εισάγετε τίτλο ταινίας <input name="Text1" type="text" /><br />
+<br />
+Επιλέξτε είδος ταινίας
+<select name="Select1">
+<option value="Drama">Drama</option>
+<option value="Comedy">Comedy</option>
+<option value="Horror">Horror</option>
+</select><br />
+	<br />
+	<br />
+<input name="Submit1" type="submit" value="Αναζήτηση" /></div>
+</form>
+
+
+
+
+</body>
+
+</html>
---
+++ search.php
@@ -0,0 +1,38 @@
+﻿<?php
+error_reporting(E_ERROR | E_PARSE);
+extract($_POST);
+
+$onoma=$_POST['Text1'];
+$eidos=$_POST['Select1'];
+
+
+$movies = array(
+
+"Comedy" => array("The Hangover","Knocked Up","Due Date"),
+
+"Drama" => array("The Shawshank Redemption","The Green Mile","Requiem for a Dream"),
+
+"Horror" => array("Halloween","The Conjuring","The Shining")
+
+);
+
+echo "Αποτελέσματα <br> ";
+echo "-------------------------------------- <br>";
+
+foreach ($movies as $obj_key =>$movie)
+{
+
+if ($obj_key!=$eidos)
+ continue;
+
+foreach ($movie as $key=>$value)
+{
+ $pos = strpos($value,$onoma);
+ if ($pos!==false)
+  echo $value,"<br>";
+}
+echo "<br>";
+}
+
+
+?>
